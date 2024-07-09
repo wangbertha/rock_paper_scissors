3de67ddcc9cca633522f2d027b8c1d991493006d
wangbertha <wangbertha@gmail.com>
2024-07-09 17:55:52 -0500

    Create console-based game for rock-paper-scissors
    Take prompts as inputs and create playRound and playGame functions

---
+++ rock_paper_scissors.js
@@ -0,0 +1,67 @@
+const options = ['rock','paper','scissors']
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * options.length)]
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt('Enter your choice of "rock", "paper", or "scissors".')
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+        return humanChoice
+    }
+    else {
+        return 'Invalid choice'
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    let result = 'lose'
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice === computerChoice) {
+        result = null
+    }
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        result = 'win'
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        result = 'win'
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        result = 'win'
+    }
+    if (result === 'win') {
+        humanScore++
+        console.log(`You win! ${capitalizeFirstLetter(humanChoice)} beats ${computerChoice}`)
+    }
+    else if (result === 'lose') {
+        computerScore++
+        console.log(`You lose! ${capitalizeFirstLetter(computerChoice)} beats ${humanChoice}`)
+    }
+    else {
+        console.log(`You both chose ${humanChoice}. It's a tie! Play again.`)
+    }
+}
+
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+}
+
+function playGame(numRounds) {
+    humanScore = 0
+    computerScore = 0
+    for (let i=0; i<numRounds; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        if (humanSelection === 'Invalid choice') {
+            console.log("Round cancelled; choice was invalid.")
+        }
+        else playRound(humanSelection, computerSelection)
+    }
+    console.log(`Final score: You ${humanScore} vs. Computer ${computerScore}`)
+    if (humanScore > computerScore) console.log("You win!")
+    else if (humanScore === computerScore) console.log("It's a tie!")
+    else console.log("You lose!")
+}
